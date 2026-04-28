d76966dc1b6a902adff1bc2e48428ec43d38dbf2
dimas pradipto <dpradipto8@gmail.com>
2026-04-28 22:17:33 +0700

    feat: create FasilitasKesehatan and ProfilIbu models, migrations, and controllers

---
+++ app/Http/Controllers/FasilitasKesehatanController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FasilitasKesehatan;
+use Illuminate\Http\Request;
+
+class FasilitasKesehatanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FasilitasKesehatan $fasilitasKesehatan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FasilitasKesehatan $fasilitasKesehatan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FasilitasKesehatan $fasilitasKesehatan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FasilitasKesehatan $fasilitasKesehatan)
+    {
+        //
+    }
+}
---
+++ database/migrations/2026_04_28_011824_create_profil_ibus_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profil_ibus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('fasilitas_kesehatan_id')->constrained()->cascadeOnDelete();
+            $table->string('nik');
+            $table->string('nama_lengkap');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('golongan_darah')->nullable();
+            $table->string('pendidikan')->nullable();
+            $table->string('pekerjaan')->nullable();
+            $table->string('agama')->nullable();
+            $table->text('alamat')->nullable();
+            $table->string('nomor_wa')->nullable();
+            $table->string('nomor_jkn')->nullable();
+            $table->string('nama_puskesmas')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profil_ibus');
+    }
+};
