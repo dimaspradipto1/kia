497c2e721192234fe747342b70116d1cbbc053f2
dimas pradipto <dpradipto8@gmail.com>
2026-04-28 08:14:31 +0700

    feat: initialize user model, migration, and application service provider configuration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('roles');
+            $table->string('status')->default('is_active');
+            $table->string('photo')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
